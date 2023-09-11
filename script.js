11cc9039faaf9439c073671453144aad1a5d9cc0
KseniaShamina <126622222+KseniaShamina@users.noreply.github.com>
2023-09-11 11:32:40 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,64 @@
+const arr = [1, 5, 4, 10, 0,];
+const even = arr.filter (el => el > 10);
+
+console.log(arr);
+
+const array = [1, 5, 4, 10, 0, 3];
+array,indexOf(2);
+
+let numbers = [1, 3, 5, 10, 20];
+numbers = numbers.goin('');
+
+console.log(numbers);
+
+let data = [
+   [1, 1, 1],
+   [1, 1, 1],
+   [1, 1, 1],
+];
+
+let items = [1, 1, 1];
+items.push('2, 2, 2');
+console.log(items);
+
+let items = [9, 8, 7, 'a', 6, 5];
+items.pop(3);
+items.sort();
+console.log(items);
+
+let riddle = [9, 8, 7, 6, 5];
+prompt('Введите число');
+if (condition) {
+    
+} else {
+    
+}
+
+let letters = [a, b, c, d, e, f];
+for (let item of letters) {
+    console.log(item);
+}
+
+const data1 = [1, 2, 3];
+const data2 = [4, 5, 6];
+
+const all = [...data1, ...data2];
+all;
+
+const arr = [1, 2, 4, 5, 7];
+for (let i=0; i<arr.length; i++);
+const result = arr.reduce((intermediate, value) => intermediate + value);
+console.log(result);
+
+const numbers = [2, 4, 6, 8];
+const twice = numbers.map (el => el * %2);
+
+const words = ['слово', '', 'слог', 'длинное предложение', 'буква'];
+console.log(words.length);
+
+function filterPositive(array) {
+    
+  }
+  
+  filterPositive([-1, 0, 5, -10, 56]);
+  filterPositive([-25, 25, 0, -1000, -2]);
