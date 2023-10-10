405e34fad19cafbf3e218cb32b461c345a28d458
KseniaShamina <126622222+KseniaShamina@users.noreply.github.com>
2023-10-10 13:38:29 -0400

    hw6

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const b = [1, 5, 4, 10, 0];
 for (let i = 0; i<b.length; i+= 1) {
     console.log(b[i])
-    if (b == 10) {
+    if (b === 10) {
         break;
     }
 }
@@ -10,16 +10,16 @@ const array = [1, 5, 4, 10, 0, 3];
 array.indexOf(2);
 
 let numbers = [1, 3, 5, 10, 20];
-numbers = numbers.going('');
+numbers = numbers.join(' ');
 
 console.log(numbers);
 
 let arr = [];
-for (let i = 1) {
+for (let i = 0; i<3; i++) {
     arr[i] = [];
 
-    for (let j = 1) {
-        arr.[i][j];
+    for (let j = 0; j<3; j++) {
+        arr[i][j]=1;
     }
 }
 
@@ -29,20 +29,21 @@ let items = [1, 1, 1];
 items.push(2, 2, 2);
 console.log(items);
 
-let items = [9, 8, 7, 'a', 6, 5];
-items.sort();
-items.pop();
-console.log(items);
+let items1 = [9, 8, 7, 'a', 6, 5];
+items1.sort();
+items1.pop();
+console.log(items1);
 
-let arr = [9, 8, 7, 6, 5];
-prompt = ('Введите число');
-if (arr.includes(9, 8, 7, 6, 5)) {
+let arr1 = [9, 8, 7, 6, 5];
+let num = prompt('Введите число');
+if (arr1.includes(+num)) {
     alert ('Угадал');
 } else {
     alert ('Не угадал');
 }
 
-const str = ['abcdef'];
+
+const str = 'abcdef';
 const reverse = str.split('').reverse().join('');
 console.log(reverse);
 
@@ -50,37 +51,40 @@ const data1 = [1, 2, 3];
 const data2 = [4, 5, 6];
 
 const all = [...data1, ...data2];
-all;
+console.log(all);
 
-const arr = [1, 2, 4, 5, 7];
-for (let numbers of arr) {
-    console.log(numbers);
+const arr2 = [1, 2, 4, 5, 7];
+for (let i=0; i<arr2.length-1; i++) {
+    console.log(arr2[i]+arr2[i+1]);
 }
 
 function twiceNumbers() {
-let arr = [];
-arr = arr.map(el => el **2);
+let arr3 = [];
+arr = arr3.map(el => el **2);
 
-console.log(arr);
+console.log(arr3);
 }
 
 function getLengthWords() {
-    let arr = [];
-    arr = arr.length;
+    let arr = ['asdf', 'rt'];
+    arr = arr.map(el => el.length);
 
     console.log(getLengthWords);
 }
 
 function filterPositive(array) {
-    if (array[i]<0) {
+    let arr = [1, 3, -2, 5];
+    for ( i = 0; i < arr.length; i++) {
+    if (arr[i]<0) {
         console.log (array[i]);
   }
 }
 
+
 console.log(filterPositive);
 
-let arr = [9, 5, 8, 1, 3];
-for (let i = 1; i<arr.length; i+1); {
-    result += arr[i] + arr[i - 1];
+let arr4 = [9, 5, 8, 1, 3];
+for (let i = 1; i<arr4.length; i++); {
+    let result = arr4[i] + arr4[i - 1];
+    console.log(result)
 }
-console.log(result);
